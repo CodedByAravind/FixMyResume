4361723736fac4bddb9372aff36fab2828f74c09
Aravind <aravindns.2k02@gmail.com>
2026-05-15 16:43:58 +0530

    Created initial user database model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
+from app.database.db import engine
+from app.database.base import Base
+
+from app.models.user import User
+
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
-    return {"message": "FixMyResume backend running"}
+    return {"message": "FixMyResume Backend Running"}
+
+@app.get("/test-db")
+def test_db():
+    return {"message": "Database connected successfully"}
